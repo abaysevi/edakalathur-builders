a635990ff7d283b067e5f7b0cac5147772c4e548
ank-ith <Ankith@techcloud.global>
2026-07-29 17:52:23 +0530

    map updated

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -124,13 +124,13 @@ function Contact() {
               >
                 <iframe
                   title="Edakkalathur Construction Location"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.0!2d76.23!3d10.63!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTDCsDM4JzAuMCJOIDc2wrAxMyc0OC4wIkU!5e0!3m2!1sen!2sin!4v1"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3921.8314486402164!2d76.15779810000001!3d10.5923479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7ebf4daa5a4c5%3A0xabaf27ea6c6be808!2sEdakkalathur%20Construction!5e0!3m2!1sen!2sin!4v1785327411867!5m2!1sen!2sin"
                   width="100%"
                   height="200"
                   style={{ border: 0, borderRadius: '12px' }}
                   allowFullScreen
                   loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
+                  referrerPolicy="strict-origin-when-cross-origin"
                 ></iframe>
               </motion.div>
             </motion.div>
